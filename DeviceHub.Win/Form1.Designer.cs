b79c8c6940beecc741806666cf34cdb16ee86785
pap25 <150640609+pap25@users.noreply.github.com>
2026-06-22 15:54:22 +0800

    阶段性开发

--- DeviceHub.Win/Form1.Designer.cs
+++ DeviceHub.Win/Form1.Designer.cs
@@ -30,10 +30,22 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             dataGridView1 = new DataGridView();
+            Column8 = new DataGridViewCheckBoxColumn();
+            状态 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
             tabControl1 = new TabControl();
             tabPage5 = new TabPage();
             pictureBox1 = new PictureBox();
             tabPage1 = new TabPage();
+            label9 = new Label();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            textBox3 = new TextBox();
             label5 = new Label();
             comboBox4 = new ComboBox();
             button1 = new Button();
@@ -43,6 +55,10 @@
             label1 = new Label();
             comboBox1 = new ComboBox();
             tabPage2 = new TabPage();
+            label10 = new Label();
+            label11 = new Label();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
             textBox2 = new TextBox();
             label3 = new Label();
             button5 = new Button();
@@ -56,6 +72,10 @@
             dataGridViewTextBoxColumn8 = new DataGridViewTextBoxColumn();
             dataGridViewTextBoxColumn9 = new DataGridViewTextBoxColumn();
             tabPage4 = new TabPage();
+            label12 = new Label();
+            label13 = new Label();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
             textBox4 = new TextBox();
             label7 = new Label();
             comboBox3 = new ComboBox();
@@ -65,14 +85,6 @@
             dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
             dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
             dataGridViewTextBoxColumn4 = new DataGridViewTextBoxColumn();
-            Column8 = new DataGridViewCheckBoxColumn();
-            状态 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column4 = new DataGridViewTextBoxColumn();
-            Column5 = new DataGridViewTextBoxColumn();
-            Column6 = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             tabControl1.SuspendLayout();
             tabPage5.SuspendLayout();
@@ -92,6 +104,50 @@
             dataGridView1.Name = "dataGridView1";
             dataGridView1.Size = new Size(1047, 454);
             dataGridView1.TabIndex = 0;
+            // 
+            // Column8
+            // 
+            Column8.HeaderText = "全选";
+            Column8.Name = "Column8";
+            Column8.Width = 40;
+            // 
+            // 状态
+            // 
+            状态.HeaderText = "状态";
+            状态.Name = "状态";
+            状态.Width = 80;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "类型";
+            Column3.Name = "Column3";
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "原始报文";
+            Column1.Name = "Column1";
+            Column1.Width = 240;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "解码结果";
+            Column2.Name = "Column2";
+            Column2.Width = 240;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "条形码";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "创建时间";
+            Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            Column6.HeaderText = "失败原因";
+            Column6.Name = "Column6";
             // 
             // tabControl1
             // 
@@ -126,6 +182,10 @@
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(label9);
+            tabPage1.Controls.Add(label8);
+            tabPage1.Controls.Add(textBox5);
+            tabPage1.Controls.Add(textBox3);
             tabPage1.Controls.Add(label5);
             tabPage1.Controls.Add(comboBox4);
             tabPage1.Controls.Add(button1);
@@ -143,10 +203,44 @@
             tabPage1.Text = "接收仪器数据队列";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(672, 35);
+            label9.Name = "label9";
+            label9.Size = new Size(20, 17);
+            label9.TabIndex = 12;
+            label9.Text = "至";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(510, 35);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 17);
+            label8.TabIndex = 11;
+            label8.Text = "创建时间";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(692, 32);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(96, 23);
+            textBox5.TabIndex = 10;
+            textBox5.Text = "2016-06-22";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(571, 32);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 9;
+            textBox3.Text = "2016-06-22";
+            // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(209, 37);
+            label5.Location = new Point(164, 34);
             label5.Name = "label5";
             label5.Size = new Size(32, 17);
             label5.TabIndex = 8;
@@ -156,14 +250,14 @@
             // 
             comboBox4.FormattingEnabled = true;
             comboBox4.Items.AddRange(new object[] { "全部", "检验结果", "查询样本申请信息" });
-            comboBox4.Location = new Point(247, 32);
+            comboBox4.Location = new Point(202, 29);
             comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(121, 25);
+            comboBox4.Size = new Size(91, 25);
             comboBox4.TabIndex = 7;
             // 
             // button1
             // 
-            button1.Location = new Point(905, 34);
+            button1.Location = new Point(918, 29);
             button1.Name = "button1";
             button1.Size = new Size(129, 23);
             button1.TabIndex = 6;
@@ -173,7 +267,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(486, 34);
+            textBox1.Location = new Point(356, 31);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(148, 23);
             textBox1.TabIndex = 5;
@@ -181,7 +275,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(406, 37);
+            label2.Location = new Point(306, 34);
             label2.Name = "label2";
             label2.Size = new Size(44, 17);
             label2.TabIndex = 4;
@@ -189,7 +283,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(769, 31);
+            button2.Location = new Point(824, 29);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 3;
@@ -211,11 +305,15 @@
             comboBox1.Items.AddRange(new object[] { "全部", "待处理", "已处理", "处理失败" });
             comboBox1.Location = new Point(54, 29);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 25);
+            comboBox1.Size = new Size(89, 25);
             comboBox1.TabIndex = 1;
             // 
             // tabPage2
             // 
+            tabPage2.Controls.Add(label10);
+            tabPage2.Controls.Add(label11);
+            tabPage2.Controls.Add(textBox6);
+            tabPage2.Controls.Add(textBox7);
             tabPage2.Controls.Add(textBox2);
             tabPage2.Controls.Add(label3);
             tabPage2.Controls.Add(button5);
@@ -230,9 +328,43 @@
             tabPage2.Text = "发送仪器数据队列";
             tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(587, 21);
+            label10.Name = "label10";
+            label10.Size = new Size(20, 17);
+            label10.TabIndex = 16;
+            label10.Text = "至";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(425, 21);
+            label11.Name = "label11";
+            label11.Size = new Size(56, 17);
+            label11.TabIndex = 15;
+            label11.Text = "创建时间";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(607, 18);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(96, 23);
+            textBox6.TabIndex = 14;
+            textBox6.Text = "2016-06-22";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(486, 18);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 13;
+            textBox7.Text = "2016-06-22";
+            // 
             // textBox2
             // 
-            textBox2.Location = new Point(323, 18);
+            textBox2.Location = new Point(249, 16);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(148, 23);
             textBox2.TabIndex = 12;
@@ -240,7 +372,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(243, 21);
+            label3.Location = new Point(199, 18);
             label3.Name = "label3";
             label3.Size = new Size(44, 17);
             label3.TabIndex = 11;
@@ -248,7 +380,7 @@
             // 
             // button5
             // 
-            button5.Location = new Point(606, 15);
+            button5.Location = new Point(768, 18);
             button5.Name = "button5";
             button5.Size = new Size(75, 23);
             button5.TabIndex = 10;
@@ -268,7 +400,7 @@
             // 
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "全部", "待处理", "已处理", "处理失败" });
-            comboBox2.Location = new Point(79, 17);
+            comboBox2.Location = new Point(53, 14);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(121, 25);
             comboBox2.TabIndex = 8;
@@ -317,6 +449,10 @@
             // 
             // tabPage4
             // 
+            tabPage4.Controls.Add(label12);
+            tabPage4.Controls.Add(label13);
+            tabPage4.Controls.Add(textBox8);
+            tabPage4.Controls.Add(textBox9);
             tabPage4.Controls.Add(textBox4);
             tabPage4.Controls.Add(label7);
             tabPage4.Controls.Add(comboBox3);
@@ -331,9 +467,43 @@
             tabPage4.Text = "操作日志";
             tabPage4.UseVisualStyleBackColor = true;
             // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(562, 20);
+            label12.Name = "label12";
+            label12.Size = new Size(20, 17);
+            label12.TabIndex = 20;
+            label12.Text = "至";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(400, 20);
+            label13.Name = "label13";
+            label13.Size = new Size(56, 17);
+            label13.TabIndex = 19;
+            label13.Text = "创建时间";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(582, 17);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(96, 23);
+            textBox8.TabIndex = 18;
+            textBox8.Text = "2016-06-22";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(461, 17);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 17;
+            textBox9.Text = "2016-06-22";
+            // 
             // textBox4
             // 
-            textBox4.Location = new Point(270, 18);
+            textBox4.Location = new Point(234, 17);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(148, 23);
             textBox4.TabIndex = 16;
@@ -341,7 +511,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(220, 21);
+            label7.Location = new Point(196, 21);
             label7.Name = "label7";
             label7.Size = new Size(32, 17);
             label7.TabIndex = 15;
@@ -368,7 +538,7 @@
             // 
             // button4
             // 
-            button4.Location = new Point(614, 18);
+            button4.Location = new Point(704, 21);
             button4.Name = "button4";
             button4.Size = new Size(75, 23);
             button4.TabIndex = 10;
@@ -398,50 +568,6 @@
             // 
             dataGridViewTextBoxColumn4.HeaderText = "时间";
             dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            // 
-            // Column8
-            // 
-            Column8.HeaderText = "全选";
-            Column8.Name = "Column8";
-            Column8.Width = 40;
-            // 
-            // 状态
-            // 
-            状态.HeaderText = "状态";
-            状态.Name = "状态";
-            状态.Width = 80;
-            // 
-            // Column3
-            // 
-            Column3.HeaderText = "类型";
-            Column3.Name = "Column3";
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "原始报文";
-            Column1.Name = "Column1";
-            Column1.Width = 240;
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "解码结果";
-            Column2.Name = "Column2";
-            Column2.Width = 240;
-            // 
-            // Column4
-            // 
-            Column4.HeaderText = "条形码";
-            Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            Column5.HeaderText = "创建时间";
-            Column5.Name = "Column5";
-            // 
-            // Column6
-            // 
-            Column6.HeaderText = "失败原因";
-            Column6.Name = "Column6";
             // 
             // Form1
             // 
@@ -512,5 +638,17 @@
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
         private DataGridViewTextBoxColumn Column6;
+        private Label label9;
+        private Label label8;
+        private TextBox textBox5;
+        private TextBox textBox3;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label12;
+        private Label label13;
+        private TextBox textBox8;
+        private TextBox textBox9;
     }
 }
--- DeviceHub.Win/doc/schema.sql
+++ DeviceHub.Win/doc/schema.sql
@@ -1,45 +1,37 @@
-create table instrument (
+/*create table instrument (
     id bigint not null auto_increment comment '主键ID',
     instrument_code varchar(50) not null comment '仪器编号',
     instrument_name varchar(100) not null comment '仪器名称',
-    manufacturer varchar(100) default null comment '生产厂商',
-    model varchar(100) default null comment '仪器型号',
-    status tinyint not null default 1 comment '状态：1启用，0停用',
+    model varchar(100) not null comment '仪器型号',
+    manufacturer varchar(100) not null comment '生产厂商',
+    status tinyint not null not comment '状态(1:启用，0:停用)',
     create_time bigint not null comment '创建时间',
     update_time bigint not null comment '更新时间',
     primary key(id),
     unique key uk_instrument_code(instrument_code)
-) engine=innodb default charset=utf8mb4 comment '仪器基础信息';
+) engine=innodb default charset=utf8mb4 comment '仪器基础信息(此表在LIS业务系统)';*/
 
-
-create table instrument_config (
+create table instrument_auth_code (
     id bigint not null auto_increment comment '主键ID',
-    instrument_id bigint not null comment '仪器ID',
-    protocol_type varchar(20) not null comment '通信协议：ASTM、HL7、自定义',
-    connect_type varchar(20) not null comment '连接方式：SERIAL、TCP_CLIENT、TCP_SERVER',
-    port_name varchar(20) default null comment '串口名称',
-    baud_rate int default null comment '波特率',
-    data_bits int default null comment '数据位',
-    parity varchar(10) default null comment '校验位',
-    stop_bits int default null comment '停止位',
-    ip varchar(100) default null comment 'TCP地址',
-    port int default null comment 'TCP端口',
-    enabled tinyint not null default 1 comment '是否启用：1启用，0停用',
+    instrument_id bigint not null comment '仪器ID（关联仪器基础信息）',
+    auth_code varchar(64) not null comment '仪器授权码（用于通信认证）',
+    status tinyint not null comment '状态：1有效 0失效',
+    expire_time bigint comment '过期时间（时间戳）',
+    --last_active_time bigint comment '最后通信时间',
     create_time bigint not null comment '创建时间',
     update_time bigint not null comment '更新时间',
-    primary key(id),
-    key idx_instrument_id(instrument_id)
-) engine=innodb default charset=utf8mb4 comment '仪器通信配置表';
-
+    primary key (id),
+    unique key uk_instrument_id(instrument_id),
+    unique key uk_auth_code(auth_code)
+) engine=innodb default charset=utf8mb4 comment '仪器授权码表';
 
 create table instrument_item_mapping (
     id bigint not null auto_increment comment '主键ID',
     instrument_id bigint not null comment '仪器ID',
     instrument_item_code varchar(50) not null comment '仪器项目编码',
-    instrument_item_name varchar(100) default null comment '仪器项目名称',
+    instrument_item_name varchar(100) not null comment '仪器项目名称',
     lis_item_code varchar(50) not null comment 'LIS项目编码',
-    lis_item_name varchar(100) default null comment 'LIS项目名称',
-    enabled tinyint not null default 1 comment '是否启用：1启用，0停用',
+    enabled tinyint not null comment '是否启用(1:启用，0:停用)',
     create_time bigint not null comment '创建时间',
     update_time bigint not null comment '更新时间',
     primary key(id),
@@ -47,10 +39,29 @@ create table instrument_item_mapping (
     key idx_lis_item_code(lis_item_code)
 ) engine=innodb default charset=utf8mb4 comment '仪器项目映射表';
 
-create table receive_raw_message (
+create table instrument_config (
     id bigint not null auto_increment comment '主键ID',
     instrument_id bigint not null comment '仪器ID',
-    status tinyint not null comment '状态：0待处理，1处理成功，2处理失败',
+    protocol_type varchar(20) not null comment '通信协议：ASTM、HL7、自定义',
+    connect_type varchar(20) not null comment '连接方式：Serial、TcpClient、TcpServer',
+    port_name varchar(20) not null comment '串口名称',
+    baud_rate int not null comment '波特率',
+    data_bits int not null comment '数据位',
+    parity varchar(10) not null comment '校验位',
+    stop_bits int not null comment '停止位',
+    ip varchar(100) not null comment 'TCP地址',
+    port int not null comment 'TCP端口',
+    enabled tinyint not null comment '是否启用(1:启用, 0:停用)',
+    create_time bigint not null comment '创建时间',
+    update_time bigint not null comment '更新时间',
+    primary key(id),
+    unique key uk_instrument_id(instrument_id)
+) engine=innodb default charset=utf8mb4 comment '仪器通信配置表';
+
+create table receive_message (
+    id bigint not null auto_increment comment '主键ID',
+    instrument_id bigint not null comment '仪器ID',
+    status tinyint not null comment '状态(0:待处理, 1:处理成功, 2:处理失败)',
     --call_time bigint not null comment '任务执行触发时间',
     --retry_count tinyint not null comment '重试次数',
     error_message varchar(500) not null comment '处理失败原因',
@@ -58,34 +69,35 @@ create table receive_raw_message (
     primary key(id),
     key idx_instrument_id(instrument_id),
     key idx_status(status)
-) engine=innodb default charset=utf8mb4 comment '接收仪器原始消息队列表';
+) engine=innodb default charset=utf8mb4 comment '接收仪器消息队列表';
 
-create table receive_raw_message_large (
-    receive_raw_message_id bigint not null comment '接收仪器原始消息队列表Id',
+create table receive_message_large (
+    receive_message_id bigint not null comment '接收仪器消息队列表Id',
     raw_message longtext not null comment '原始报文',
-    primary key(receive_raw_message_id)
-) engine=innodb default charset=utf8mb4 comment '接收仪器原始消息队列表扩展表';
+    primary key(receive_message_id)
+) engine=innodb default charset=utf8mb4 comment '接收仪器消息队列表扩展表';
 
 
 create table receive_message_decode (
     id bigint not null auto_increment comment '主键ID',
-    receive_raw_message_id bigint not null comment '接收仪器原始消息队列表Id',
-    type tinyint not null comment '类型：0检验结果，1查询样本申请信息',
+    receive_message_id bigint not null comment '接收仪器消息队列表Id',
+    type tinyint not null comment '类型(0:检验结果, 1:查询样本申请信息)',
     barcode varchar(50) not null comment '条形码',
-    result_json longtext not null comment '解析结果JSON',
-    status tinyint not null comment '状态：0待处理，1处理成功，2处理失败',
+    result_json longtext not null comment '解码结果JSON',
+    --status tinyint not null comment '状态：0待处理，1处理成功，2处理失败',
     create_time bigint not null comment '创建时间',
     update_time bigint not null comment '更新时间',
     primary key(id),
-    unique key uk_receive_raw_message_id(receive_raw_message_id)
-) engine=innodb default charset=utf8mb4 comment '仪器解析结果表';
+    unique key uk_receive_message_id(receive_message_id)
+) engine=innodb default charset=utf8mb4 comment '接收仪器消息解码结果表';
 
 
 create table send_message (
     id bigint not null auto_increment comment '主键ID',
     instrument_id bigint not null comment '仪器ID',
-    type tinyint not null comment '类型：0申请单',
-    status tinyint not null comment '状态：0待处理，1处理成功，2处理失败',
+    type tinyint not null comment '类型(0:申请单)',
+    barcode varchar(50) not null comment '条形码',
+    status tinyint not null comment '状态(0:待处理，1:处理成功，2:处理失败)',
     create_time bigint not null comment '创建时间',
     update_time bigint not null comment '更新时间',
     primary key(id),
@@ -95,16 +107,16 @@ create table send_message (
 create table send_message_large (
     send_message_id bigint not null comment '发送仪器消息队列表Id',
     send_json longtext not null comment '发送内容JSON',
-    primary key(receive_raw_message_id)
-) engine=innodb default charset=utf8mb4 comment '发送仪器json消息扩展表';
+    primary key(send_message_id)
+) engine=innodb default charset=utf8mb4 comment '发送仪器消息队列表扩展表';
 
 create table send_message_encoder (
     id bigint not null auto_increment comment '主键ID',
     send_message_id bigint not null comment '发送仪器消息队列表Id',
     send_content longtext not null comment '发送报文内容',
-    status tinyint not null comment '状态：0待处理，1处理成功，2处理失败',
+    --status tinyint not null comment '状态(0:待处理，1:处理成功，2:处理失败)',
     --ack_time bigint default null comment '仪器确认时间',
-    error_message varchar(500) not null comment '失败原因',
+    --error_message varchar(500) not null comment '失败原因',
     create_time bigint not null comment '创建时间',
     update_time bigint not null comment '更新时间', 
     primary key(id),
@@ -130,7 +142,7 @@ create table instrument_runtime (
 create table client_log (
     id bigint not null auto_increment comment '主键ID',
     instrument_id bigint default null comment '仪器ID',
-    level tinyint not null comment '状态(0:普通，1:警告，2:错误)',
+    level tinyint not null comment '状态(0:普通，1:警告，2:异常)',
     message longtext default null comment '日志内容',
     create_time bigint not null comment '创建时间',
     primary key(id)
