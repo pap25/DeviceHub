be323a71888d04f7cf1a19ed265191a4e1660045
pap25 <150640609+pap25@users.noreply.github.com>
2026-07-07 19:14:19 +0800

    开发

--- DeviceHub.YhloTestSerialPort/Handler/SendHandler.cs
+++ DeviceHub.YhloTestSerialPort/Handler/SendHandler.cs
@@ -1,9 +1,12 @@
 ﻿using DeviceHub.Base.Common;
 using DeviceHub.Lis;
+using DeviceHub.Lis.Dto;
 using DeviceHub.Lis.Impl;
 using DeviceHub.Model.Entities;
 using DeviceHub.Repository.Repositories;
 using DeviceHub.Service;
+using DeviceHub.YhloTestSerialPort.Protocol;
+using System.Text.Json;
 
 namespace DeviceHub.YhloTestSerialPort.Handler
 {
@@ -29,10 +32,40 @@ namespace DeviceHub.YhloTestSerialPort.Handler
         }
         public void HandleTask(SendMessage task)
         {
+            try
+            {
+                SendMessageLarge? receiveMessageLarge = sendMessageLargeRepository.GetByReceiveMessageId(task.Id).GetAwaiter().GetResult();
+                long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                if (receiveMessageLarge == null)
+                {
+                    MarkFailed(task.Id, "数据异常", now);
+                    return;
+                }
+                GetSampleApplyItemOutput? getSampleApplyItemOutput = JsonSerializer.Deserialize<GetSampleApplyItemOutput>(receiveMessageLarge.SendJson);
+                if (getSampleApplyItemOutput == null)
+                {
+                    MarkFailed(task.Id, "数据异常", now);
+                    return;
+                }
+                string sendContent = AstmMessageEncoder.EncoderIssueApplication(getSampleApplyItemOutput);
 
-            // 发送 发时候先看看在收没，没有收的话在发
-            // 保存 send_message_encoder
-            
+                // 发送 发时候先看看在收没，没有收的话在发
+                // 保存 send_message_encoder
+            }
+            catch (Exception e)
+            {
+                MarkFailed(task.Id, "HandleTask异常" + e.Message, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            }
+        }
+
+        private void MarkFailed(long id, string errorMessage, long now)
+        {
+            sendMessageRepository.UpdateStatusAndErrorMessageAndUpdateTimeById(
+                id,
+                SendMessage.StatusEnum.Failed,
+                errorMessage,
+                now).GetAwaiter().GetResult();
+            Logger.Warn(logType, $"消息处理失败 id={id}: {errorMessage}");
         }
     }
 }
---
+++ DeviceHub.YhloTestSerialPort/Protocol/AstmMessageEncoder.cs
@@ -0,0 +1,22 @@
+﻿using DeviceHub.Lis.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static DeviceHub.Yhlo.Protocol.AstmMessageDecode;
+
+namespace DeviceHub.YhloTestSerialPort.Protocol
+{
+    public class AstmMessageEncoder
+    {
+        public static string EncoderIssueApplication(GetSampleApplyItemOutput sampleApplyItem)
+        {
+            // SampleQuery TestResult
+            return "";
+        }
+        public static string EncoderRequestApplication(Object frameDataList)
+        {
+            // SampleQuery TestResult
+            return "";
+        }
+    }
+}
