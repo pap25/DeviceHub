4a0dad1571aac37995d5437ba83c81ee648b6083
pap25 <150640609+pap25@users.noreply.github.com>
2026-07-10 09:33:46 +0800

    增加NotifyTask

---
+++ DeviceHub.Utils/NotifyTask.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading;
+
+namespace DeviceHub.Utils
+{
+    public interface INotifyTaskHandler
+    {
+        void HandleTask();
+    }
+
+    public class NotifyTask : IConsumeTask
+    {
+        private readonly INotifyTaskHandler _taskHandler;
+        private readonly Thread _thread;
+        private readonly object _lockObj = new object();
+        private readonly int _waitTimeoutMilliseconds;
+
+        private volatile bool _isNewTask;
+
+        public NotifyTask(INotifyTaskHandler taskHandler)
+            : this(taskHandler, "default", 60 * 1000)
+        {
+        }
+
+        public NotifyTask(INotifyTaskHandler taskHandler, string handleType, int waitTimeoutMilliseconds)
+        {
+            _taskHandler = taskHandler;
+            _waitTimeoutMilliseconds = waitTimeoutMilliseconds;
+
+            _thread = new Thread(ConsumeLoop)
+            {
+                IsBackground = true,
+                Name = $"{handleType}_notify_task"
+            };
+        }
+
+        public void StartConsume()
+        {
+            _thread.Start();
+        }
+
+        public void NotifyConsume()
+        {
+            lock (_lockObj)
+            {
+                _isNewTask = true;
+                Monitor.Pulse(_lockObj);
+            }
+        }
+
+        public void Shutdown()
+        {
+            _thread.Interrupt();
+        }
+
+        private void ConsumeLoop()
+        {
+            while (true)
+            {
+                _isNewTask = false;
+                _taskHandler.HandleTask();
+
+                lock (_lockObj)
+                {
+                    if (_isNewTask)
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        Monitor.Wait(_lockObj, _waitTimeoutMilliseconds);
+                    }
+                    catch (ThreadInterruptedException ex)
+                    {
+                        Logger.Info(nameof(NotifyTask),
+                            $"interrupted notify task threadName:{Thread.CurrentThread.Name}, errorMsg:{ex.Message}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
