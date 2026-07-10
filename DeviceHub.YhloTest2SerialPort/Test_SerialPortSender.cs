0714d1ad5877f4de10547074ee86d930065fdc77
pap25 <150640609+pap25@users.noreply.github.com>
2026-07-10 14:08:16 +0800

    test

---
+++ DeviceHub.YhloTest2SerialPort/Test_SerialPortSender.cs
@@ -0,0 +1,68 @@
+﻿using DeviceHub.Base.Constant;
+using DeviceHub.Utils;
+
+namespace DeviceHub.YhloTest2SerialPort
+{
+    public class Test_SerialPortSender : INotifyTaskHandler
+    {
+        private readonly string logType = nameof(Test_SerialPortSender);
+        private readonly SerialPortSession session;
+        private readonly ISenderTaskHandler senderTaskHandler;
+
+        private List<byte[]> sendFrameList = [];
+        private int sendFrameOffset;
+
+        public Test_SerialPortSender(SerialPortSession session, ISenderTaskHandler senderTaskHandler)
+        {
+            this.session = session;
+            this.senderTaskHandler = senderTaskHandler;
+        }
+
+        public IReadOnlyList<byte[]> SendFrameList => sendFrameList;
+
+        public void ClearSendFrames()
+        {
+            sendFrameList = [];
+            sendFrameOffset = 0;
+        }
+
+        public void HandleTask()
+        {
+            switch (session.GetLineStateUnlocked())
+            {
+                case LineState.Idle:
+                    sendFrameList = senderTaskHandler.SearchEncoderTask();
+                    if (sendFrameList.Count > 0)
+                    {
+                        Logger.Debug(logType, $"待发送帧条数: {sendFrameList.Count}");
+                        sendFrameOffset = 0;
+                        session.SendUnlocked(ASTMProtocols.ENQ);
+                        session.SetLineStateUnlocked(LineState.WaitingAck);
+                    }
+                    break;
+
+                case LineState.WaitingAck:
+                    break;
+
+                case LineState.Sending:
+                    if (sendFrameList.Count > sendFrameOffset)
+                    {
+                        byte[] frame = sendFrameList[sendFrameOffset];
+                        sendFrameOffset++;
+                        session.SendFrameUnlocked(frame);
+                    }
+                    else if (sendFrameList.Count > 0)
+                    {
+                        senderTaskHandler.Completed(sendFrameList);
+                        ClearSendFrames();
+                        session.SetLineStateUnlocked(LineState.Idle);
+                        session.SendUnlocked(ASTMProtocols.EOT);
+                    }
+                    break;
+
+                case LineState.Receiving:
+                    break;
+            }
+        }
+    }
+}
