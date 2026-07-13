540cd32e19b213ea2e9b609e0c3c5aab7b5a6e7d
pap25 <150640609+pap25@users.noreply.github.com>
2026-07-13 19:01:47 +0800

    开发

--- DeviceHub.YhloTestTcpServer/Handler/SendHandler.cs
+++ DeviceHub.YhloTestTcpServer/Handler/SendHandler.cs
@@ -1,23 +1,81 @@
-﻿using DeviceHub.Utils;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DeviceHub.Lis.Dto;
+using DeviceHub.Model.Entities;
+using DeviceHub.Repository.Repositories;
+using DeviceHub.Service;
+using DeviceHub.Utils;
+using DeviceHub.YhloTestTcpServer.Protocol;
+using System.Text.Json;
 
 namespace DeviceHub.YhloTestTcpServer.Handler
 {
-    public class SendHandler : IBatchTaskHandler<Object>
+    public class SendHandler : IBatchTaskHandler<SendMessage>
     {
-        public void HandleTask(object task)
+        private readonly string logType = nameof(SendHandler);
+        private long _instrumentId;
+        private readonly SendMessageRepository sendMessageRepository = SendMessageRepository.Instance;
+        private readonly SendMessageLargeRepository sendMessageLargeRepository = SendMessageLargeRepository.Instance;
+        private readonly SendMessageService sendMessageService = SendMessageService.Instance;
+
+        public SendHandler(long instrumentId)
         {
-            // 发送
-            // 保存 send_message_encoder
-            throw new NotImplementedException();
+            _instrumentId = instrumentId;
         }
 
-        public IEnumerable<object> SearchTask()
+        public IEnumerable<SendMessage> SearchTask()
         {
-            // 查询 send_message
-            throw new NotImplementedException();
+            List<SendMessage> taskList = sendMessageRepository
+                .FindByInstrumentIdAndStatusOrderAsc(_instrumentId, SendMessage.StatusEnum.Pending, 15).GetAwaiter().GetResult();
+            return taskList;
+        }
+        public void HandleTask(SendMessage task)
+        {
+            try
+            {
+                SendMessageLarge? receiveMessageLarge = sendMessageLargeRepository.GetBySendMessageId(task.Id).GetAwaiter().GetResult();
+                long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                if (receiveMessageLarge == null)
+                {
+                    MarkFailed(task.Id, "数据异常", now);
+                    return;
+                }
+
+                if (task.Type == SendMessage.TypeEnum.RequestApplication)
+                {
+                    GetSampleApplyItemOutput? getSampleApplyItemOutput = JsonSerializer.Deserialize<GetSampleApplyItemOutput>(receiveMessageLarge.SendJson);
+                    if (getSampleApplyItemOutput == null)
+                    {
+                        MarkFailed(task.Id, "数据异常", now);
+                        return;
+                    }
+
+                    byte[] rawMessage = Hl7MessageEncoder.EncoderRequestApplication(getSampleApplyItemOutput);
+
+                    // 发送
+                    sendMessageService.UpdateSuccessRequestApplication(task.Id, rawMessage).GetAwaiter();
+                }
+                else if (task.Type == SendMessage.TypeEnum.IssueApplication)
+                {
+
+                }
+                else
+                {
+                    MarkFailed(task.Id, "不支持的类型 " + task.Type, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+                }
+            }
+            catch (Exception e)
+            {
+                MarkFailed(task.Id, "HandleTask异常" + e.Message, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            }
+        }
+
+        private void MarkFailed(long id, string errorMessage, long now)
+        {
+            sendMessageRepository.UpdateStatusAndErrorMessageAndUpdateTimeById(
+                id,
+                SendMessage.StatusEnum.Failed,
+                errorMessage,
+                now).GetAwaiter().GetResult();
+            Logger.Warn(logType, $"消息处理失败 id={id}: {errorMessage}");
         }
     }
 }
---
+++ DeviceHub.YhloTestTcpServer/Protocol/Hl7MessageEncoder.cs
@@ -0,0 +1,15 @@
+﻿using DeviceHub.Lis.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeviceHub.YhloTestTcpServer.Protocol
+{
+    public class Hl7MessageEncoder
+    {
+        public static byte[] EncoderRequestApplication(GetSampleApplyItemOutput getSampleApplyItemOutput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
