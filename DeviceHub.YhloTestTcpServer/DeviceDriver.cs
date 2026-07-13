f965cb16fa750fda6197e57b7ea554c846a28fb4
pap25 <150640609+pap25@users.noreply.github.com>
2026-07-13 11:04:19 +0800

    开发

---
+++ DeviceHub.YhloTestTcpServer/DeviceDriver.cs
@@ -0,0 +1,50 @@
+﻿using DeviceHub.Abstractions;
+using DeviceHub.Abstractions.Dto;
+using DeviceHub.Lis.Dto;
+using DeviceHub.Model.Entities;
+using DeviceHub.Utils;
+using DeviceHub.YhloTestTcpServer.Handler;
+
+namespace DeviceHub.YhloTestTcpServer
+{
+    public class DeviceDriver : ITcpDeviceDriver
+    {
+        private readonly string logType = nameof(DeviceDriver);
+        private IConsumeTask receiveTask = null!;
+        private IConsumeTask lisIssueApplication = null!;
+        private TcpServerSession session;
+
+        public DeviceDriver()
+        {
+            session = new TcpServerSession();
+        }
+
+        public async Task Start(long instrumentId, TcpConfig config)
+        {
+            receiveTask = new BatchConsumeTask<ReceiveMessage>(new ReceiveHandler(instrumentId));
+            receiveTask.StartConsume();
+
+            lisIssueApplication = new BatchConsumeTask<GetSampleApplyListOutput>(new LisIssueApplication(instrumentId));
+            lisIssueApplication.StartConsume();
+
+            await session.Start(instrumentId, config);
+
+            Logger.Info(logType, $"设备驱动已启动 instrumentId={instrumentId}, host={config.Host}, port={config.Port}");
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void NotifyLisIssueApplication()
+        {
+            lisIssueApplication.NotifyConsume();
+        }
+
+        public string GetClientRemoteEndPoint()
+        {
+            return session.GetClientRemoteEndPoint();
+        }
+    }
+}
---
+++ DeviceHub.YhloTestTcpServer/Handler/ReceiveHandler.cs
@@ -0,0 +1,44 @@
+﻿using DeviceHub.Utils;
+using DeviceHub.Base.Transports;
+using DeviceHub.Model.Entities;
+using DeviceHub.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DeviceHub.YhloTestTcpServer.Handler
+{
+    public class ReceiveHandler : IBatchTaskHandler<ReceiveMessage>
+    {
+        private long _instrumentId;
+        private readonly ReceiveMessageRepository receiveMessageRepository = ReceiveMessageRepository.Instance;
+        private readonly ReceiveMessageLargeRepository receiveMessageLargeRepository = ReceiveMessageLargeRepository.Instance;
+        public ReceiveHandler(long instrumentId)
+        {
+            _instrumentId = instrumentId;
+        }
+        public IEnumerable<ReceiveMessage> SearchTask()
+        {
+            List<ReceiveMessage> taskList = receiveMessageRepository
+                .FindByInstrumentIdAndStatusOrderAsc(_instrumentId, ReceiveMessage.StatusEnum.Pending, 15).GetAwaiter().GetResult();
+            return taskList;
+        }
+        public void HandleTask(ReceiveMessage task)
+        {
+            // 调用接口上传检验结果
+            // 解码后保存 receive_message_decode 更新receive_message
+            ReceiveMessageLarge? receiveMessageLarge = receiveMessageLargeRepository.GetByReceiveMessageId(task.Id).GetAwaiter().GetResult();
+            if (receiveMessageLarge == null)
+            {
+                long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                //receiveMessageRepository.UpdateStatusAndErrorMessageAndUpdateTimeById(task.Id, ReceiveMessage.StatusEnum.Failed, "数据异常", now);
+                return;
+            }
+            byte[] rawMessage = receiveMessageLarge.RawMessage;
+
+
+            //receiveMessageRepository.UpdateStatusAndUpdateTimeById(task.Id, ReceiveMessage.StatusEnum.Success, now);
+        }
+    }
+}
