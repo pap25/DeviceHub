489f0a179d3fec41d260fa4e39a1766f1cd0c291
pap25 <150640609+pap25@users.noreply.github.com>
2026-07-15 13:41:32 +0800

    开发

--- DeviceHub.Template/Template/LisIssueApplicationHandler.cs
+++ DeviceHub.Template/Template/LisIssueApplicationHandler.cs
@@ -7,7 +7,7 @@ using DeviceHub.Repository.Repositories;
 using DeviceHub.Service;
 using System.Text.Json;
 
-namespace DeviceHub.YhloTestV2SerialPort.Handler
+namespace DeviceHub.Template.Template
 {
     public class LisIssueApplicationHandler : IBatchTaskHandler<GetSampleApplyListOutput>
     {
@@ -16,10 +16,12 @@ namespace DeviceHub.YhloTestV2SerialPort.Handler
         private readonly ILisClient lisClient = LisClient.Instance;
         private readonly SendMessageService sendMessageService = SendMessageService.Instance;
         private readonly DictionaryRepository dictionaryRepository = DictionaryRepository.Instance;
+        private readonly IConsumeTask? sendHandlerTask;
 
-        public LisIssueApplicationHandler(long instrumentId)
+        public LisIssueApplicationHandler(long instrumentId, IConsumeTask sendHandlerTask)
         {
-            _instrumentId = instrumentId;
+            this._instrumentId = instrumentId;
+            this.sendHandlerTask = sendHandlerTask;
         }
 
         public IEnumerable<GetSampleApplyListOutput> SearchTask()
@@ -59,6 +61,8 @@ namespace DeviceHub.YhloTestV2SerialPort.Handler
             {
                 Logger.Warn(logType, $"保存LIS拉取下单申请异常 id={task.Id}: {ex.Message}");
             }
+
+            sendHandlerTask?.NotifyConsume();
         }
     }
 }
